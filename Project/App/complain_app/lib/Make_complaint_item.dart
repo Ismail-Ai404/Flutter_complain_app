dbda76c19c5bb2efb6a2d9d437b2d68c02bb22ec
Smilexix_ <ayon000777@gmail.com>
2022-02-21 19:16:24 +0600

    Route to Send_complaint screen

--- Project/App/complain_app/lib/Make_complaint_item.dart
+++ Project/App/complain_app/lib/Make_complaint_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/send_complain_screen.dart';
 
 class MakeComplaintItem extends StatelessWidget {
   final String name;
@@ -11,21 +12,31 @@ class MakeComplaintItem extends StatelessWidget {
     @required this.name,
   });
 
+  void selectComplain(BuildContext ctx) {
+    Navigator.of(ctx)
+        .push(MaterialPageRoute(builder: (_) => SendComplainScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Text(name, style: Theme.of(context).textTheme.bodyText2),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectComplain(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Text(name, style: Theme.of(context).textTheme.bodyText2),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
       ),
     );
   }
---
+++ Project/App/complain_app/lib/screens/send_complain_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SendComplainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 15,
+        title: const Text('Let\'s send that complaint'),
+      ),
+    );
+  }
+}

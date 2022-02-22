0c325e9a58d0bf049511a01052c3613a8849a291
Smilexix_ <ayon000777@gmail.com>
2022-02-22 18:35:27 +0600

    Add search bar with no functinality

--- Project/App/complain_app/lib/screens/make_complaint_screen.dart
+++ Project/App/complain_app/lib/screens/make_complaint_screen.dart
@@ -6,6 +6,8 @@ import '../Make_complaint_item.dart';
 final items = ['Beer', 'Meraaj'];
 const title = 'Who to complain against';
 
+String search;
+
 class MakeComplaintScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,23 +19,55 @@ class MakeComplaintScreen extends StatelessWidget {
         elevation: 15,
         title: const Text('Looking for who to complain'),
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_SEARCH
-            .map(
-              (catData) => MakeComplaintItem(
-                color: catData.color,
-                name: catData.name,
-                dept: 'ECE',
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              onSubmitted: (val) {
+                search = val;
+              },
+              cursorColor: Color.fromARGB(255, 31, 3, 12),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: '',
+                icon: Icon(
+                  Icons.person_search,
+                  color: Color.fromARGB(255, 82, 0, 27),
+                  size: 24.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
               ),
-            )
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            height: 300,
+            child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20,
+
+              padding: const EdgeInsets.all(25),
+              children: DUMMY_SEARCH
+                  .map(
+                    (catData) => MakeComplaintItem(
+                      color: catData.color,
+                      name: catData.name,
+                      dept: 'ECE',
+                    ),
+                  )
+                  .toList(),
+
+              // gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              //   maxCrossAxisExtent: 200,
+              //   childAspectRatio: 3 / 2,
+              //   crossAxisSpacing: 20,
+              //   mainAxisSpacing: 20,
+              // ),
+            ),
+          ),
+        ],
       ),
     );
   }

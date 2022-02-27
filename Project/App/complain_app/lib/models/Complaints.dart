0136b46e9142c81e5e6e867bf5e37684336f46ac
Smilexix_ <ayon000777@gmail.com>
2022-02-27 16:41:10 +0600

    Add class model for complants table

---
+++ Project/App/complain_app/lib/models/Complaints.dart
@@ -0,0 +1,30 @@
+import 'dart:html';
+
+import 'package:flutter/foundation.dart';
+
+enum Status {
+  Processing,
+  Finished,
+}
+
+class Complaints {
+  final String lodgerID;
+  final String reviewID;
+  final String accussedID;
+  final String complaintID;
+  final bool isBehalf;
+  final AudioElement speech;
+  final File evidence;
+  final Status status;
+
+  Complaints({
+    @required this.lodgerID,
+    @required this.reviewID,
+    @required this.accussedID,
+    @required this.complaintID,
+    @required this.evidence,
+    @required this.isBehalf,
+    @required this.speech,
+    @required this.status,
+  });
+}

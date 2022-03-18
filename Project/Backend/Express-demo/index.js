1bb08c7217a2b7f8010abf5a504f8eebfd3b010c
Smilexix_ <ayon000777@gmail.com>
2022-03-18 18:37:06 +0600

    How post works

--- Project/Backend/Express-demo/index.js
+++ Project/Backend/Express-demo/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const Joi = require("joi");
+app.use(express.json());
 
 const courses = [
      {
@@ -31,6 +33,22 @@ app.get("/api/courses/:id", (req, res) => {
      const course = courses.find((c) => c.id === parseInt(req.params.id));
      if (!course) res.status(404).send("Given ID not found");
      else res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+     if (!req.body.name || req.body.name.length < 3) {
+          res.status(400).send(
+               "Name is required and shpuld be greather than 3 "
+          );
+          return;
+     }
+
+     const course = {
+          id: courses.length + 1,
+          name: req.body.name,
+     };
+     courses.push(course);
+     res.send(course);
 });
 // Environment variable : PORT
 const port = process.env.PORT || 3000;

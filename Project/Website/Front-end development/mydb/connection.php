24b3ec093254a62f0de9c43b5df193ca55add649
Mashfiqul Faruque <mashfiqul.faruque@northsouth.edu>
2022-05-05 21:00:13 +0600

    Fixing minor things of Api

--- Project/Website/Front-end development/mydb/connection.php
+++ Project/Website/Front-end development/mydb/connection.php
@@ -1,8 +1,8 @@
 <?php
 
 try {
-    $connection = new PDO('mysql:host=localhost,dbname=id18887272_cmsusers', 'id18887272_users', '12345@Dihan@');
-    $connection->setAttribute(PDO::ATTR_ERRMODE, POD::ERRMODE_EXCEPTION);
+    $connection = new PDO('mysql:host=localhost;dbname=id18887272_cmsusers', 'id18887272_users', '12345@Dihan@');
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "yes connected";
 } catch (PODException $exc) {
     echo $exc->getMessage();
